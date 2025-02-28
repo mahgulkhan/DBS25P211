69f947da91fa5c314c32b99124e2c87b90edd0b6
Mah Gul Khan <2024CS211@student.uet.edu.pk>
2025-02-28 23:59:42 +0500

    Add files via upload

---
+++ DBS25P211/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.header = new System.Windows.Forms.Label();
+            this.header2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // header
+            // 
+            this.header.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.header.AutoSize = true;
+            this.header.BackColor = System.Drawing.Color.Transparent;
+            this.header.Font = new System.Drawing.Font("Old English Text MT", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.header.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.header.Location = new System.Drawing.Point(291, 65);
+            this.header.MaximumSize = new System.Drawing.Size(900, 100);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(825, 44);
+            this.header.TabIndex = 0;
+            this.header.Text = "University of Engineering and Technology Lahore";
+            // 
+            // header2
+            // 
+            this.header2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.header2.AutoSize = true;
+            this.header2.BackColor = System.Drawing.Color.CadetBlue;
+            this.header2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.header2.Font = new System.Drawing.Font("Perpetua Titling MT", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.header2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.header2.Location = new System.Drawing.Point(471, 160);
+            this.header2.MaximumSize = new System.Drawing.Size(600, 450);
+            this.header2.Name = "header2";
+            this.header2.Size = new System.Drawing.Size(512, 46);
+            this.header2.TabIndex = 1;
+            this.header2.Text = "Faculty Management";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(607, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 82);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Click to Proceed";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Task1.Properties.Resources.Navy_large_retro_geometric_shape_wallpaper_mural;
+            this.ClientSize = new System.Drawing.Size(1385, 610);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.header2);
+            this.Controls.Add(this.header);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label header;
+        private System.Windows.Forms.Label header2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DBS25P211/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3d9da50d-347f-4f8e-ac5b-fac990c10a55")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
